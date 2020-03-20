8aeae1006e3b551353789024567aff7d14815114
Error <Error>
2020-03-20 11:54:46 -0400

    Fix Broken URL

--- flare-install.sh
+++ flare-install.sh
@@ -6,8 +6,8 @@ sudo chown -R $USER:$USER /opt/flare
 
 # Download required files
 wget -c -O /opt/flare/flare-config.yml https://raw.githubusercontent.com/careylabvt/flare-containers/flare-push-test/flare-config.yml
-wget -c -O /opt/flare/flare-host.sh https://raw.githubusercontent.com/flare-containers/flare-push-test/flare-host.sh
-wget -c -O /opt/flare/flare-container.sh https://raw.githubusercontent.com/flare-containers/flare-push-test/flare-container.sh
+wget -c -O /opt/flare/flare-host.sh https://raw.githubusercontent.com/caeylabvt/flare-containers/flare-push-test/flare-host.sh
+wget -c -O /opt/flare/flare-container.sh https://raw.githubusercontent.com/careylabvt/flare-containers/flare-push-test/flare-container.sh
 
 # Add execution permission to scripts
 chmod +x /opt/flare/*.sh
