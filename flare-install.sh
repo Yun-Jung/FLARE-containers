4ee27ec9894e90db96b0e0ca53480f6c9ee23e48
Error <Error>
2020-03-21 14:39:14 -0400

    Update Installer

--- flare-install.sh
+++ flare-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Setup Flare Directory on the Host
-sudo mkdir -p /opt/flare
+sudo mkdir -p /opt/flare/shared
 sudo chown -R $USER:$USER /opt/flare
 
 # Get Required Files
 sudo wget -O /usr/bin/yq https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_amd64
-wget -O /opt/flare/flare-config.yml https://raw.githubusercontent.com/CareyLabVT/FLARE-containers/flare-push-test/flare-config.yml
+wget -O /opt/flare/shared/flare-config.yml https://raw.githubusercontent.com/CareyLabVT/FLARE-containers/flare-push-test/flare-config.yml
 wget -O /opt/flare/flare-host.sh https://raw.githubusercontent.com/CareyLabVT/FLARE-containers/flare-push-test/flare-host.sh
-chmod +x /usr/bin/yq /opt/flare/flare-host.sh
+sudo chmod +x /usr/bin/yq /opt/flare/flare-host.sh
