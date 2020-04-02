da9f4ed27c4a6b3b98e83b0010bc62e89908c7ec
Error <Error>
2020-04-01 23:22:10 -0400

    Add Autotest

--- flare-install.sh
+++ flare-install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+source flare-install.conf
+
+sudo apt -y install wget
+
 # Setup Flare Directory on the Host
 sudo mkdir -p /opt/flare/shared
 sudo chown -R $USER:$USER /opt/flare
 
 # Get Required Files
-sudo wget -O /usr/bin/yq https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_amd64
-wget -O /opt/flare/shared/flare-config.yml https://raw.githubusercontent.com/CareyLabVT/FLARE-containers/flare-push-test/flare-config.yml
-wget -O /opt/flare/flare-host.sh https://raw.githubusercontent.com/CareyLabVT/FLARE-containers/flare-push-test/flare-host.sh
+sudo wget -O /usr/bin/yq $YQ_EXEC
+wget -O /opt/flare/shared/flare-config.yml $FLARE_CONFIG
+wget -O /opt/flare/flare-host.sh $FLARE_HOST
 sudo chmod +x /usr/bin/yq /opt/flare/flare-host.sh
+
