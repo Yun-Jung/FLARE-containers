59c01a686fe43595654464376a23bce7261c6f72
vahid-dan <v.daneshmand@gmail.com>
2020-12-18 13:52:19 -0500

    Add Library Import

--- fcre/04_visualize.R
+++ fcre/04_visualize.R
@@ -1,5 +1,6 @@
 #saved_file <- "/Users/quinn/Dropbox (VTFRS)/Research/SSC_forecasting/run_flare_package/wrr_runs_obs/wrr_runs_H_2019_03_14_2019_03_15_F_16_20201124T085027.nc"
 #qaqc_data_location <- "/Users/quinn/Dropbox/Research/SSC_forecasting/FLARE_package/flare_lake_examples/fcre/qaqc_data/"
+lirary(tibble)
 
 lake_directory <- "/root/flare/fcre"
 forecast_location <- "/root/flare/shared/flare-generate-forecast-2.0-dev/forecast"
